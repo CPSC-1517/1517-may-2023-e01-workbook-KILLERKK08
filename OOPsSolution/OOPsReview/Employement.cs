e1d5d717cb0079d35cf6ed0f27521f467ed40a4f
Khushman Kapoor <kkapoor1@nait.ca>
2023-05-17 17:20:29 -0600

    created constructor

---
+++ OOPsSolution/OOPsReview/Employement.cs
@@ -0,0 +1,68 @@
+﻿namespace OOPsReview
+{
+    public class Employement
+    {
+        private SupervisoryLevel _Level;
+        private string _Title;
+        private double _Years;
+
+        public string Title
+        {
+            get { return _Title; }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is required");
+                }
+                _Title = value;
+            }
+        }
+        public SupervisoryLevel Level
+        {
+            get { return _Level; }
+            private set
+            {
+                if (!Enum.IsDefined(typeof(SupervisoryLevel), value))
+                {
+                    throw new ArgumentException($"Supervisory level is invalid: {value}");
+                }
+                _Level = value;
+            }
+        }
+        public double Years
+        {
+            get { return Years; }
+            set
+            {
+                //if (value < 0)
+                if (!Utilities.IsZeroOrPositive(value))
+                {
+                    throw new ArgumentOutOfRangeException ("value");
+                }
+                _Years = value;
+            }
+        }
+        public DateTime StartDate { get; private set; }
+
+        public Employement ()
+        {
+            Title = "Unknown";
+            Level = SupervisoryLevel.TeamMember;
+            StartDate = DateTime.Today;
+        }
+        public Employement(string title, SupervisoryLevel level, DateTime startdate, double years = 0.0)
+        {
+            Title = title;
+            Level = level;
+            Years = years;
+
+            if (startdate >= DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"The start date{startdate} cannot be in future");
+            }
+            StartDate = startdate;
+        }
+
+    }
+}
---
+++ OOPsSolution/OOPsReview/SupervisoryLevel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public enum SupervisoryLevel
+    {
+        /// <summary>
+        /// by default enum names are strings representing an integer value
+        /// by default the integer values start at zero and increment by one
+        /// one could assign their own value to each of the enum names
+        /// </summary>
+        Entry,
+        TeamMember,
+        TeamLeader,
+        Supervisor,
+        DepartmentHead,
+        Owner
+    }
+}
