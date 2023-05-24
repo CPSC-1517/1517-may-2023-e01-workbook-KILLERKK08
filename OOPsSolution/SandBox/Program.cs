ae228c30a8858031fb2ce7597563c6374e327d7a
Khushman Kapoor <kkapoor1@nait.ca>
2023-05-24 17:45:15 -0600

    added a console application

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using OOPsReview;
+Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
+
+myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
