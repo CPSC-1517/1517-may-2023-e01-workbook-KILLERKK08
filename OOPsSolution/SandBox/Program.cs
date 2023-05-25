57ed86fd19fea14b246c4b011b3f062f5e3c20d1
Khushman Kapoor <kkapoor1@nait.ca>
2023-05-24 18:18:48 -0600

    refactory

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -5,3 +5,39 @@ Console.WriteLine(myHome.ToString());
 
 myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
+
+bool flag = false;
+if (myHome.Province.ToLower() == "ab")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "bc")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "sk")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "sk":
+    case "bc":
+    case "mn":
+        {
+            flag = true;
+            break;
+        }
+        default:
+        {
+            flag = false;
+            break;
+        }
+
+}
