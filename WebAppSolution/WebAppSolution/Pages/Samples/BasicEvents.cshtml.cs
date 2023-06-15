e5efc3e8d22a11e11b5de7ec804d2207b1a50db3
Khushman Kapoor <130007057+KILLERKK08@users.noreply.github.com>
2023-06-14 18:29:12 -0600

    added a samples folder

---
+++ WebAppSolution/WebAppSolution/Pages/Samples/BasicEvents.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppSolution.Pages.Samples
+{
+    public class BasicEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
