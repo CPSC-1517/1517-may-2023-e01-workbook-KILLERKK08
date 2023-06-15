d900a2a41727c04d137bc088b6b9c56b99ebb2aa
Khushman Kapoor <130007057+KILLERKK08@users.noreply.github.com>
2023-06-14 18:45:17 -0600

    made some changes in the code

--- WebAppSolution/WebAppSolution/Pages/Samples/BasicEvents.cshtml.cs
+++ WebAppSolution/WebAppSolution/Pages/Samples/BasicEvents.cshtml.cs
@@ -5,8 +5,11 @@ namespace WebAppSolution.Pages.Samples
 {
     public class BasicEventsModel : PageModel
     {
+        public string FeedBack { get; set; }
+
         public void OnGet()
         {
+            FeedBack = "in OnGet";
         }
     }
 }
